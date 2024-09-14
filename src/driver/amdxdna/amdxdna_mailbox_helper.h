836f950ba35ef9b5193ec19c2458d6b1a7fafb07
Min Ma <min.ma@amd.com>
2024-09-13 17:48:41 -0700

    support response with status not at first word
    Signed-off-by: Min Ma <min.ma@amd.com>

--- src/driver/amdxdna/amdxdna_mailbox_helper.h
+++ src/driver/amdxdna/amdxdna_mailbox_helper.h
@@ -37,6 +37,8 @@ struct xdna_notify {
 		.notify_cb = xdna_msg_cb,			\
 	}
 
+#define XDNA_STATUS_OFFSET(name) (offsetof(struct name##_resp, status) / sizeof(u32))
+
 int xdna_msg_cb(void *handle, const u32 *data, size_t size);
 int xdna_send_msg_wait(struct amdxdna_dev *xdna, struct mailbox_channel *chann,
 		       struct xdna_mailbox_msg *msg);
