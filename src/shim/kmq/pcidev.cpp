a1023dd921de65da12e8342641a549b509f01c13
Max Zhen <max.zhen@amd.com>
2024-11-27 14:57:56 -0800

    Allow smaller dev heap
    Signed-off-by: Max Zhen <max.zhen@amd.com>

--- src/shim/kmq/pcidev.cpp
+++ src/shim/kmq/pcidev.cpp
@@ -8,7 +8,8 @@
 namespace {
 
 // Device memory heap needs to be within one 64MB page. The maximum size is 64MB.
-const size_t dev_mem_size = (64 << 20);
+const size_t max_heap_mem_size = (64 << 20);
+const size_t min_heap_mem_size = (1 << 20);
 
 }
 
@@ -32,14 +33,29 @@ pdev_kmq::
 create_device(xrt_core::device::handle_type handle, xrt_core::device::id_type id) const
 {
   auto dev = std::make_shared<device_kmq>(*this, handle, id);
-  try {
-    // Alloc device memory on first device creation.
-    // No locking is needed since driver will ensure only one heap BO is created.
-    if (m_dev_heap_bo == nullptr)
-      m_dev_heap_bo = std::make_unique<bo_kmq>(*dev, dev_mem_size, AMDXDNA_BO_DEV_HEAP);
-  } catch (const xrt_core::system_error& ex) {
-    if (ex.get_code() != EBUSY)
-      throw;
+  size_t heap_sz = max_heap_mem_size;
+
+  while (m_dev_heap_bo == nullptr) {
+    try {
+      // Alloc device memory on first device creation.
+      // No locking is needed since driver will ensure only one heap BO is created.
+      m_dev_heap_bo = std::make_unique<bo_kmq>(*dev, heap_sz, AMDXDNA_BO_DEV_HEAP);
+    } catch (const xrt_core::system_error& ex) {
+      switch (ex.get_code()) {
+      case EBUSY:
+        if (m_dev_heap_bo == nullptr)
+          shim_err(EINVAL, "Leaking dev heap BO?");
+        break;
+      case ENOMEM:
+        // Try with smaller size in case of memory pressure or IOMMU_MODE constrain
+        heap_sz /= 2;
+        if (heap_sz < min_heap_mem_size)
+          shim_err(EINVAL, "No mem for dev heap BO, giving up");
+        break;
+      default:
+        throw;
+      }
+    }
   }
   return dev;
 }
