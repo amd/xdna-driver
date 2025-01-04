49c3e0be661d087ef0647779b5a35d39a5476af5
Min Ma <min.ma@amd.com>
2025-01-03 17:21:18 -0800

    update copyright
    Signed-off-by: Min Ma <min.ma@amd.com>

--- src/driver/amdxdna/amdxdna_devel.h
+++ src/driver/amdxdna/amdxdna_devel.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (C) 2024, Advanced Micro Devices, Inc.
+ * Copyright (C) 2024-2025, Advanced Micro Devices, Inc.
  */
 
 #ifndef _AMDXDNA_DEVEL_
--- src/driver/amdxdna/amdxdna_drm.h
+++ src/driver/amdxdna/amdxdna_drm.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (C) 2022-2024, Advanced Micro Devices, Inc.
+ * Copyright (C) 2022-2025, Advanced Micro Devices, Inc.
  */
 
 #ifndef _AMDXDNA_DRM_H_
