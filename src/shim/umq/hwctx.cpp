0854f80d9b8811f6833b761fbfab75fc82aafc7f
Hayden Laccabue <hlaccabu@amd.com>
2025-01-15 10:11:06 -0800

    Fixing umq log metadata to be inside umq src files (#353)
    Signed-off-by: Hayden Laccabue <hlaccabu@amd.com>

--- src/shim/umq/hwctx.cpp
+++ src/shim/umq/hwctx.cpp
@@ -12,8 +12,9 @@ namespace shim_xdna {
 hw_ctx_umq::
 hw_ctx_umq(const device& device, const xrt::xclbin& xclbin, const xrt::hw_context::qos_type& qos)
   : hw_ctx(device, qos, std::make_unique<hw_q_umq>(device, 8), xclbin)
+  , m_metadata()
 {
-  hw_ctx::init_log_buf();
+  init_log_buf();
   hw_ctx::create_ctx_on_device();
 
   shim_debug("Created UMQ HW context (%d)", get_slotidx());
@@ -23,6 +24,7 @@ hw_ctx_umq::
 ~hw_ctx_umq()
 {
   shim_debug("Destroying UMQ HW context (%d)...", get_slotidx());
+  fini_log_buf();
 }
 
 std::unique_ptr<xrt_core::buffer_handle>
@@ -33,6 +35,43 @@ alloc_bo(void* userptr, size_t size, uint64_t flags)
   auto& dev = const_cast<device&>(get_device());
 
   return dev.alloc_bo(userptr, AMDXDNA_INVALID_CTX_HANDLE, size, flags);
+}
+
+void
+hw_ctx_umq::
+init_log_buf()
+{
+  size_t column_size = 1024;
+  auto log_buf_size = hw_ctx::m_num_cols * column_size + sizeof(m_metadata);
+  hw_ctx::m_log_bo = alloc_bo(nullptr, log_buf_size, XCL_BO_FLAGS_EXECBUF);
+  m_log_buf = hw_ctx::m_log_bo->map(xrt_core::buffer_handle::map_type::write);
+  uint64_t bo_paddr = hw_ctx::m_log_bo->get_properties().paddr;
+  set_metadata(hw_ctx::m_num_cols, column_size, bo_paddr, UMQ_DEBUG_BUFFER);
+  std::memset(m_log_buf, 0, log_buf_size);
+  std::memcpy(m_log_buf, &m_metadata, sizeof(m_metadata));
+}
+
+void
+hw_ctx_umq::
+fini_log_buf(void)
+{
+  if (hw_ctx::m_log_bo)
+    hw_ctx::m_log_bo->unmap(m_log_buf);
+}
+
+void
+hw_ctx_umq::
+set_metadata(int num_cols, size_t size, uint64_t bo_paddr, enum umq_log_flag flag)
+{
+  m_metadata.magic_no = LOG_MAGIC_NO;
+  m_metadata.major = 0;
+  m_metadata.minor = 1;
+  m_metadata.umq_log_flag = flag;
+  m_metadata.num_cols = num_cols;
+  for (int i = 0; i < num_cols; i++) {
+    m_metadata.col_paddr[i] = bo_paddr + size * i + sizeof(m_metadata);
+    m_metadata.col_size[i] = size;
+  }
 }
 
 } // shim_xdna
