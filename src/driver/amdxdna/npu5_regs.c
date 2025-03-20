7d9532a646ea46a64fcc60bc3a731e3ec95a4004
Min Ma <min.ma@amd.com>
2025-03-20 10:37:10 -0700

    update fw for npu4/5/6; change npu.sbin to npu.dev.sbin
    Signed-off-by: Min Ma <min.ma@amd.com>

--- src/driver/amdxdna/npu5_regs.c
+++ src/driver/amdxdna/npu5_regs.c
@@ -6,7 +6,7 @@
 #include "npu4_family.h"
 
 const struct amdxdna_dev_priv npu5_dev_priv = {
-	.fw_path        = "amdnpu/17f0_11/npu.sbin",
+	.fw_path        = "amdnpu/17f0_11/npu.dev.sbin",
 	.protocol_major = 0x6,
 	.protocol_minor = 0x6,
 	NPU4_COMMON_DEV_PRIV,
