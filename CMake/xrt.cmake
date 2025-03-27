f71a73a439e96ff586cec928cfeff81ed7c23801
Max Zhen <40219623+maxzhen@users.noreply.github.com>
2025-03-27 15:04:25 -0700

    updating XRT submodule build rules (#436)
    Signed-off-by: Max Zhen <max.zhen@amd.com>

--- CMake/xrt.cmake
+++ CMake/xrt.cmake
@@ -3,18 +3,28 @@
 
 # Bring in XRT git submodule and exclude parts of XRT we don't need
 set(XRT_EXCLUDE_SUB_DIRECTORY
-  src/runtime_src/core/pcie/driver
-# Hard dependencies for xrt cmake target are introduced in master. Can't exclude below directories any more
-# src/runtime_src/core/pcie/emulation
-# src/runtime_src/xocl
-  src/runtime_src/core/pcie/tools
+  src/runtime_src/core/pcie/emulation
   src/runtime_src/core/pcie/windows
+  src/runtime_src/core/pcie/driver
+  src/runtime_src/core/pcie/tools
+  src/runtime_src/core/pcie/noop
+
   src/runtime_src/core/tools
+  src/runtime_src/core/edge
+
+  src/runtime_src/tools
+  src/runtime_src/xocl
   src/runtime_src/ert
   src/runtime_src/xrt
-  src/xma
+
   tests/validate
+  src/xma
   )
+
+# Instruct XRT to build the npu component
+set(XRT_NPU 1)
+# To be removed when controlled by XDP with XRT_NPU
+set(XDP_CLIENT_BUILD_CMAKE "yes")
 
 set(XRT_EXCLUDE_INCLUDE_FILE
   src/CMake/nativeTests.cmake
