2b8cbb2cb579ccd9d03514580457819f10c30f4c
Min Ma <min.ma@amd.com>
2025-06-19 22:25:46 -0700

    Add new shim_test for auto suspend/resume and context health report (ctx_pc) (#602)
    * Add test case for auto suspend and resume
    * Add test case to inject bad instruction to hang command and report ctx program counter
    * remove part_ctx_stop(), replaced by part_ctx_stop_wait()
    
    ---------
    
    Signed-off-by: Min Ma <min.ma@amd.com>

--- test/shim_test/io_param.h
+++ test/shim_test/io_param.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
 
 #ifndef _SHIMTEST_IO_PARAM_H_
 #define _SHIMTEST_IO_PARAM_H_
@@ -10,9 +10,11 @@ struct io_test_parameter {
 #define IO_TEST_LATENCY_PERF  1
 #define IO_TEST_THRUPUT_PERF  2
   int perf;
-#define IO_TEST_NORMAL_RUN    0
-#define IO_TEST_NOOP_RUN      1
-#define IO_TEST_BAD_RUN       2
+#define IO_TEST_NORMAL_RUN    		0
+#define IO_TEST_NOOP_RUN      		1
+#define IO_TEST_BAD_RUN       		2
+#define IO_TEST_DELAY_RUN     		3
+#define IO_TEST_BAD_RUN_REPORT_CTX_PC	4
   int type;
 #define IO_TEST_IOCTL_WAIT    0
 #define IO_TEST_POLL_WAIT     1
