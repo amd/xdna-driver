64c7c692da12b48e7fb5fcf6f3b6554f75b6e8f0
Max Zhen <40219623+maxzhen@users.noreply.github.com>
2025-07-21 10:41:25 -0700

    properly handle throw in thread (#655)
    Signed-off-by: Max Zhen <max.zhen@amd.com>

--- test/shim_test/multi_threads.h
+++ test/shim_test/multi_threads.h
@@ -19,7 +19,7 @@ public:
     m_total_threads(num_thread),
     m_test(test)
   {
-    std::cout << "Total threads " << m_total_threads << std::endl;
+    std::cout << "Total threads: " << m_total_threads << std::endl;
   }
 
   ~multi_thread()
@@ -29,20 +29,33 @@ public:
   void run_test(xrt_core::device::id_type id, std::shared_ptr<device> dev, arg_type& arg)
   {
     for (int i = 0; i < m_total_threads; i++) {
-      m_threads.push_back(std::thread([&](int i){
+      m_failed.push_back(false);
+      m_threads.push_back(
+        std::thread([&](int i) {
           std::cout << "Thread " << i << " started" << std::endl;
-	  m_test(id, dev ,arg);
-	}, i)
+          try {
+            m_test(id, dev ,arg);
+          } catch (const std::exception& ex) {
+            m_failed[i] = true;
+            std::cout << "Thread " << i << " failed: " << ex.what() << std::endl;
+          }
+        }, i)
       );
     }
 
     for (int i = 0; i < m_total_threads; i++)
       m_threads[i].join();
+
+    for (int i = 0; i < m_total_threads; i++) {
+      if (m_failed[i])
+        throw std::runtime_error("At least one thread has failed");
+    }
   }
 
 private:
   int m_total_threads;
   std::vector<std::thread> m_threads;
+  std::vector<bool> m_failed;
   func m_test;
 };
 
