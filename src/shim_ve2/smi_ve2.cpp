a99dccae9df6a2560d925453ecd0347eab048071
Manoj Takasi <mtakasi@amd.com>
2025-07-29 14:40:08 +0530

    Validate changes required for telluride
    Signed-off-by: Manoj Takasi <mtakasi@amd.com>

--- src/shim_ve2/smi_ve2.cpp
+++ src/shim_ve2/smi_ve2.cpp
@@ -6,6 +6,29 @@
 namespace shim_telluride::smi {
 
 xrt_core::smi::subcommand
+create_validate_subcommand()
+{
+  std::vector<xrt_core::smi::basic_option> validate_test_desc = {
+   {"latency", "Run end-to-end latency test", "common"}
+  };
+
+  std::map<std::string, std::shared_ptr<xrt_core::smi::option>> validate_suboptions;
+  validate_suboptions.emplace("device", std::make_shared<xrt_core::smi::option>("device", "d", "The Bus:Device.Function (e.g., 0000:d8:00.0) device of interest", "common", "", "string"));
+  validate_suboptions.emplace("format", std::make_shared<xrt_core::smi::option>("format", "f", "Report output format. Valid values are:\n"
+                                "\tJSON        - Latest JSON schema\n"
+                                "\tJSON-2020.2 - JSON 2020.2 schema", "common", "JSON", "string"));
+  validate_suboptions.emplace("output", std::make_shared<xrt_core::smi::option>("output", "o", "Direct the output to the given file", "common", "", "string"));
+  validate_suboptions.emplace("help", std::make_shared<xrt_core::smi::option>("help", "h", "Help to use this sub-command", "common", "", "none"));
+  validate_suboptions.emplace("run", std::make_shared<xrt_core::smi::listable_description_option>("run", "r", "Run a subset of the test suite. Valid options are:\n",
+                              "common", "",  "array", validate_test_desc));
+  validate_suboptions.emplace("path", std::make_shared<xrt_core::smi::option>("path", "p", "Path to the directory containing validate xclbins", "hidden", "", "string"));
+  validate_suboptions.emplace("param", std::make_shared<xrt_core::smi::option>("param", "", "Extended parameter for a given test. Format: <test-name>:<key>:<value>", "param", "", "string"));
+  validate_suboptions.emplace("elf", std::make_shared<xrt_core::smi::option>("elf", "", "Run the test in ELF mode", "hidden", "", "none"));
+
+  return {"validate", "Validates the given device by executing the platform's validate executable", "common", std::move(validate_suboptions)};
+}
+
+xrt_core::smi::subcommand
 create_examine_subcommand()
 {
     std::vector<xrt_core::smi::basic_option> examine_report_desc = {
@@ -37,6 +60,7 @@ get_smi_config()
   auto smi_instance = xrt_core::smi::instance();
 
   // Add subcommands
+  smi_instance->add_subcommand("validate", create_validate_subcommand());
   smi_instance->add_subcommand("examine", create_examine_subcommand());
 
   return smi_instance->build_json();
