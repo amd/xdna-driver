a8f2b5af32b2ea8b2e7a36ffbfcf97e9e499021e
Bikash Singha <bisingha@xcobisingha40x.xlnx.xilinx.com>
2025-07-30 16:47:45 +0530

    Fixing size alignment
    Signed-off-by: Bikash Singha <bisingha@xcobisingha40x.xlnx.xilinx.com>

--- src/driver/amdxdna/amdxdna_gem_of.c
+++ src/driver/amdxdna/amdxdna_gem_of.c
@@ -2,7 +2,7 @@
 /*
  * Copyright (C) 2025, Advanced Micro Devices, Inc.
  */
-#include <linux/kernel.h>
+
 #include <linux/dma-mapping.h>
 #include <linux/version.h>
 
@@ -40,14 +40,14 @@ struct amdxdna_gem_obj *amdxdna_gem_get_obj(struct amdxdna_client *client, u32 b
 static void amdxdna_gem_dma_obj_free(struct drm_gem_object *gobj)
 {
 	struct amdxdna_dev *xdna = to_xdna_dev(gobj->dev);
-	struct amdxdna_gem_obj *abo = to_xdna_obj(gobj);
+	struct amdxdna_gem_obj *abo = container_of(gobj, struct amdxdna_gem_obj, base.base);
 
 	XDNA_DBG(xdna, "BO type %d xdna_addr 0x%llx", abo->type, abo->mem.dev_addr);
-	printk("[bs]: %s: BO type %d xdna_addr 0x%llx", __func__, abo->type, abo->mem.dev_addr);
-	// drm_gem_dma_object_free(gobj);
 
-	if (abo->mem.kva && abo->mem.dev_addr)
+	if (abo->mem.kva && abo->mem.dev_addr) {
 		dma_free_coherent(gobj->dev->dev, abo->mem.size, abo->mem.kva, abo->mem.dev_addr);
+		abo->mem.kva = NULL;
+	}
 
 	drm_gem_object_release(gobj);
 	kfree(abo);
@@ -84,11 +84,45 @@ struct drm_gem_object *amdxdna_gem_create_object_cb(struct drm_device *dev, size
 	return to_gobj(abo);
 }
 
+static struct drm_gem_dma_object *amdxdna_cma_create(struct drm_device *dev, size_t size)
+{
+	struct drm_gem_dma_object *cma_obj;
+	struct drm_gem_object *gem_obj;
+	int ret = 0;
+
+	gem_obj = kzalloc(sizeof(struct amdxdna_gem_obj), GFP_KERNEL);
+	if (!gem_obj)
+		return ERR_PTR(-ENOMEM);
+
+	cma_obj = container_of(gem_obj, struct drm_gem_dma_object, base);
+
+	gem_obj->funcs = &amdxdna_gem_dma_funcs;
+
+	//manually init the drm gem obj
+	ret = drm_gem_object_init(dev, gem_obj, size);
+	if (ret)
+		goto error;
+
+	ret = drm_gem_create_mmap_offset(gem_obj);
+	if (ret) {
+		drm_gem_object_release(gem_obj);
+		goto error;
+	}
+
+	return cma_obj;
+
+error:
+	memset(&cma_obj->base, 0, sizeof(cma_obj->base));
+	kfree(gem_obj);
+	return ERR_PTR(ret);
+
+}
+
 static struct amdxdna_gem_obj *amdxdna_drm_create_dma_bo(struct drm_device *dev,
 							 struct amdxdna_drm_create_bo *args,
 							 struct drm_file *filp)
 {
-	struct drm_gem_dma_object *dma;
+	struct drm_gem_dma_object *cma_obj;
 	struct amdxdna_gem_obj *abo;
 	size_t size = args->size;
 	dma_addr_t dma_addr;
@@ -97,45 +131,33 @@ static struct amdxdna_gem_obj *amdxdna_drm_create_dma_bo(struct drm_device *dev,
 	/* Round up to more than 4K to ensure to allocate memory from CMA always */
 	if (size <= PAGE_SIZE)
 		size = round_up(size, 2 * PAGE_SIZE);
+	else
+		size = round_up(size, PAGE_SIZE);
 
-	// dma = drm_gem_dma_create(dev, size);
-	// if (IS_ERR(dma))
-	// 	return ERR_CAST(dma);
+	cma_obj = amdxdna_cma_create(dev, size);
+	if (IS_ERR(cma_obj))
+		return ERR_PTR(-ENOMEM);
 
 	vaddr = dma_alloc_coherent(dev->dev, size, &dma_addr, GFP_KERNEL);
-	if (!vaddr)
-		return ERR_PTR(-ENOMEM);
-
-	abo = kzalloc(sizeof(*abo), GFP_KERNEL);
-	if (!abo) {
-		dma_free_coherent(dev->dev, size, vaddr, dma_addr);
+	if (!vaddr) {
+		drm_gem_object_release(&cma_obj->base);
+		kfree(to_xdna_obj(&cma_obj->base));
+		memset(&cma_obj->base, 0 , sizeof(cma_obj->base));
 		return ERR_PTR(-ENOMEM);
 	}
 
-	dma = &abo->base;
+	cma_obj->dma_addr = dma_addr;
+	cma_obj->vaddr = vaddr;
 
-	//manually init the drm gem obj
-	if (drm_gem_object_init(dev, &dma->base, size)) {
-		dma_free_coherent(dev->dev, size, vaddr, dma_addr);
-		kfree(abo);
-		return ERR_PTR(-ENOMEM);
-	}
+	abo = to_xdna_obj(&cma_obj->base);
 
-	if (drm_gem_create_mmap_offset(&dma->base)) {
-		dma_free_coherent(dev->dev, size, vaddr, dma_addr);
-		drm_gem_object_release(&dma->base);
-		kfree(abo);
-		return ERR_PTR(-ENOMEM);
-	}
-
-	dma->base.funcs = &amdxdna_gem_dma_funcs;
-	dma->dma_addr = dma_addr;
-	dma->vaddr = vaddr;
-
-	abo->mem.dev_addr = dma->dma_addr;
-	abo->mem.kva = dma->vaddr;
+	abo->mem.dev_addr = dma_addr;
+	abo->mem.kva = vaddr;
 	abo->type = args->type;
 	abo->mem.size = size;
+
+	mutex_init(&abo->lock);
+	abo->mem.userptr = AMDXDNA_INVALID_ADDR;
 
 	return abo;
 }
