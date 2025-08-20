a0c028e98f0292301b20f92bf2058c4fdf3dd5d2
Nishad Saraf <nishads@amd.com>
2025-08-20 12:14:56 -0700

    Upgrade to the latest app health interface (#701)
    Upgrade to the latest app health interface.
    
    Signed-off-by: Nishad Saraf <nishads@amd.com>

--- src/driver/amdxdna/aie2_message.c
+++ src/driver/amdxdna/aie2_message.c
@@ -690,6 +690,9 @@ int aie2_get_app_health(struct amdxdna_dev_hdl *ndev, struct aie2_mgmt_dma_hdl *
 	dma_addr_t addr;
 	int ret;
 
+	if (!aie2_is_supported_msg(ndev, MSG_OP_GET_APP_HEALTH))
+		return -EOPNOTSUPP;
+
 	addr = aie2_mgmt_buff_get_dma_addr(mgmt_hdl);
 	if (!addr) {
 		XDNA_ERR(xdna, "Invalid DMA address: %lld", addr);
@@ -701,13 +704,15 @@ int aie2_get_app_health(struct amdxdna_dev_hdl *ndev, struct aie2_mgmt_dma_hdl *
 	req.buf_size = size;
 
 	ret = aie2_send_mgmt_msg_wait_silent(ndev, &msg);
-	if (ret) {
-		XDNA_DBG(xdna, "Get app health failed, ret 0x%x", ret);
-		return ret;
-	}
+	if (ret)
+		XDNA_DBG(xdna, "Get app health failed, ret %d", ret);
 
-	if (resp.status != AIE2_STATUS_SUCCESS) {
-		XDNA_DBG(xdna, "Get app health got status 0x%x", resp.status);
+	if (resp.status == AIE2_STATUS_MGMT_ERT_DRAM_BUFFER_SIZE_INVALID) {
+		XDNA_ERR(xdna, "Invalid buffer size(required 0x%x) for get app health cmd",
+			 resp.required_buffer_size);
+		ret = -EINVAL;
+	} else if (resp.status != AIE2_STATUS_SUCCESS) {
+		XDNA_ERR(xdna, "Get app health got status 0x%x", resp.status);
 		ret = -EINVAL;
 	}
 
