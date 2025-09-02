ae9810e276864653c6d61d86aed967b01ed58b17
Ryan Chane <Ryan.Chane@amd.com>
2025-09-02 10:01:23 -0700

    Update xrt-smi validate binaries using VTD
    Signed-off-by: Ryan Chane <Ryan.Chane@amd.com>

--- build/build.sh
+++ build/build.sh
@@ -244,11 +244,104 @@ if [[ $example == 1 ]]; then
   exit 0
 fi
 
+# Update xrt-smi validate related files from vtd
+cp -f ../vtd/xclbin_prod/validate_npu4.xclbin ../tools/bins/17f0_10/validate.xclbin
+cp -f ../vtd/xclbin_prod/validate_npu4.xclbin ../tools/bins/17f0_11/validate.xclbin
+cp -f ../vtd/xclbin_prod/validate_npu4.xclbin ../tools/bins/17f0_20/validate.xclbin
+cp -f ../vtd/xclbin_prod/validate_elf_npu4.xclbin ../tools/bins/17f0_10/validate_elf.xclbin
+cp -f ../vtd/xclbin_prod/validate_elf_npu4.xclbin ../tools/bins/17f0_11/validate_elf.xclbin
+cp -f ../vtd/xclbin_prod/validate_elf_npu4.xclbin ../tools/bins/17f0_20/validate_elf.xclbin
+cp -f ../vtd/xclbin_prod/preemption_4x4_npu4.xclbin ../tools/bins/17f0_10/preemption_4x4.xclbin
+cp -f ../vtd/xclbin_prod/preemption_4x4_npu4.xclbin ../tools/bins/17f0_11/preemption_4x4.xclbin
+cp -f ../vtd/xclbin_prod/preemption_4x4_npu4.xclbin ../tools/bins/17f0_20/preemption_4x4.xclbin
+cp -f ../vtd/xclbin_prod/preemption_4x8_npu4.xclbin ../tools/bins/17f0_10/preemption_4x8.xclbin
+cp -f ../vtd/xclbin_prod/preemption_4x8_npu4.xclbin ../tools/bins/17f0_11/preemption_4x8.xclbin
+cp -f ../vtd/xclbin_prod/preemption_4x8_npu4.xclbin ../tools/bins/17f0_20/preemption_4x8.xclbin
+cp -f ../vtd/xclbin_prod/mobilenet_elf_npu4_4x4.xclbin ../tools/bins/17f0_10/mobilenet_4col.xclbin
+cp -f ../vtd/xclbin_prod/mobilenet_elf_npu4_4x4.xclbin ../tools/bins/17f0_11/mobilenet_4col.xclbin
+cp -f ../vtd/xclbin_prod/mobilenet_elf_npu4_4x4.xclbin ../tools/bins/17f0_20/mobilenet_4col.xclbin
+cp -f ../vtd/xclbin_prod/validate_npu.xclbin ../tools/bins/1502_00/validate.xclbin
+cp -f ../vtd/elf/nop_npu4.elf ../tools/bins/17f0_10/nop.elf
+cp -f ../vtd/elf/nop_npu4.elf ../tools/bins/17f0_11/nop.elf
+cp -f ../vtd/elf/nop_npu4.elf ../tools/bins/17f0_20/nop.elf
+cp -f ../vtd/elf/preemption_noop_4x4.elf ../tools/bins/17f0_10/preemption_noop_4x4.elf
+cp -f ../vtd/elf/preemption_noop_4x4.elf ../tools/bins/17f0_11/preemption_noop_4x4.elf
+cp -f ../vtd/elf/preemption_noop_4x4.elf ../tools/bins/17f0_20/preemption_noop_4x4.elf
+cp -f ../vtd/elf/preemption_noop_4x8.elf ../tools/bins/17f0_10/preemption_noop_4x8.elf
+cp -f ../vtd/elf/preemption_noop_4x8.elf ../tools/bins/17f0_11/preemption_noop_4x8.elf
+cp -f ../vtd/elf/preemption_noop_4x8.elf ../tools/bins/17f0_20/preemption_noop_4x8.elf
+cp -f ../vtd/elf/preemption_memtile_4x4.elf ../tools/bins/17f0_10/preemption_memtile_4x4.elf
+cp -f ../vtd/elf/preemption_memtile_4x4.elf ../tools/bins/17f0_11/preemption_memtile_4x4.elf
+cp -f ../vtd/elf/preemption_memtile_4x4.elf ../tools/bins/17f0_20/preemption_memtile_4x4.elf
+cp -f ../vtd/elf/preemption_memtile_4x8.elf ../tools/bins/17f0_10/preemption_memtile_4x8.elf
+cp -f ../vtd/elf/preemption_memtile_4x8.elf ../tools/bins/17f0_11/preemption_memtile_4x8.elf
+cp -f ../vtd/elf/preemption_memtile_4x8.elf ../tools/bins/17f0_20/preemption_memtile_4x8.elf
+cp -f ../vtd/elf/mobilenet_4col.elf ../tools/bins/17f0_10/mobilenet_4col.elf
+cp -f ../vtd/elf/mobilenet_4col.elf ../tools/bins/17f0_11/mobilenet_4col.elf
+cp -f ../vtd/elf/mobilenet_4col.elf ../tools/bins/17f0_20/mobilenet_4col.elf
+cp -f ../vtd/input_data/mobilenet/mobilenet_ifm.bin ../tools/bins/Mobilenet/mobilenet_ifm.bin
+cp -f ../vtd/input_data/mobilenet/mobilenet_param.bin ../tools/bins/Mobilenet/mobilenet_param.bin
+cp -f ../vtd/input_data/mobilenet/buffer_sizes.json ../tools/bins/Mobilenet/buffer_sizes.json
+
+cp -f ../vtd/runner/throughput/phx/recipe_throughput_phx.json ../tools/bins/Runner/throughput/recipe_throughput_phx.json
+cp -f ../vtd/runner/throughput/phx/validate_throughput_phx.xclbin ../tools/bins/Runner/throughput/validate_throughput_phx.xclbin
+cp -f ../vtd/runner/throughput/phx/profile_throughput_phx.json ../tools/bins/Runner/throughput/profile_throughput_phx.json
+cp -f ../vtd/runner/throughput/strx/recipe_throughput_strx.json ../tools/bins/Runner/throughput/recipe_throughput_strx.json
+cp -f ../vtd/runner/throughput/strx/validate_throughput_strx.xclbin ../tools/bins/Runner/throughput/validate_throughput_strx.xclbin
+cp -f ../vtd/runner/throughput/strx/profile_throughput_strx.json ../tools/bins/Runner/throughput/profile_throughput_strx.json
+cp -f ../vtd/runner/throughput/strx/nop_throughput_strx.elf ../tools/bins/Runner/throughput/nop_throughput_strx.elf
+cp -f ../vtd/runner/latency/phx/recipe_latency_phx.json ../tools/bins/Runner/latency/recipe_latency_phx.json
+cp -f ../vtd/runner/latency/phx/validate_latency_phx.xclbin ../tools/bins/Runner/latency/validate_latency_phx.xclbin
+cp -f ../vtd/runner/latency/phx/profile_latency_phx.json ../tools/bins/Runner/latency/profile_latency_phx.json
+cp -f ../vtd/runner/latency/strx/recipe_latency_strx.json ../tools/bins/Runner/latency/recipe_latency_strx.json
+cp -f ../vtd/runner/latency/strx/validate_latency_strx.xclbin ../tools/bins/Runner/latency/validate_latency_strx.xclbin
+cp -f ../vtd/runner/latency/strx/profile_latency_strx.json ../tools/bins/Runner/latency/profile_latency_strx.json
+cp -f ../vtd/runner/latency/strx/nop_latency_strx.elf ../tools/bins/Runner/latency/nop_latency_strx.elf
+cp -f ../vtd/runner/df_bandwidth/phx/recipe_df_bandwidth_phx.json ../tools/bins/Runner/df_bandwidth/recipe_df_bandwidth_phx.json
+cp -f ../vtd/runner/df_bandwidth/phx/df_bw_phx.elf ../tools/bins/Runner/df_bandwidth/df_bw_phx.elf
+cp -f ../vtd/runner/df_bandwidth/phx/profile_df_bandwidth_phx.json ../tools/bins/Runner/df_bandwidth/profile_df_bandwidth_phx.json
+cp -f ../vtd/runner/df_bandwidth/phx/validate_df_bandwidth_phx.xclbin ../tools/bins/Runner/df_bandwidth/validate_df_bandwidth_phx.xclbin
+cp -f ../vtd/runner/df_bandwidth/strx/recipe_df_bandwidth_strx.json ../tools/bins/Runner/df_bandwidth/recipe_df_bandwidth_strx.json
+cp -f ../vtd/runner/df_bandwidth/strx/df_bw_strx.elf ../tools/bins/Runner/df_bandwidth/df_bw_strx.elf
+cp -f ../vtd/runner/df_bandwidth/strx/profile_df_bandwidth_strx.json ../tools/bins/Runner/df_bandwidth/profile_df_bandwidth_strx.json
+cp -f ../vtd/runner/df_bandwidth/strx/validate_df_bandwidth_strx.xclbin ../tools/bins/Runner/df_bandwidth/validate_df_bandwidth_strx.xclbin
+cp -f ../vtd/runner/gemm/strx/recipe_gemm_strx.json ../tools/bins/Runner/gemm/recipe_gemm_strx.json
+cp -f ../vtd/runner/gemm/strx/gemm_elf_strx.xclbin ../tools/bins/Runner/gemm/gemm_elf_strx.xclbin
+cp -f ../vtd/runner/gemm/strx/profile_gemm_strx.json ../tools/bins/Runner/gemm/profile_gemm_strx.json
+cp -f ../vtd/runner/gemm/strx/gemm_int8_strx.elf ../tools/bins/Runner/gemm/gemm_int8_strx.elf
+cp -f ../vtd/runner/aie_reconfig_overhead/strx/recipe_aie_reconfig_strx.json ../tools/bins/Runner/aie_reconfig_overhead/recipe_aie_reconfig_strx.json
+cp -f ../vtd/runner/aie_reconfig_overhead/strx/recipe_aie_reconfig_nop_strx.json ../tools/bins/Runner/aie_reconfig_overhead/recipe_aie_reconfig_nop_strx.json
+cp -f ../vtd/runner/aie_reconfig_overhead/strx/aie_reconfig_strx.xclbin ../tools/bins/Runner/aie_reconfig_overhead/aie_reconfig_strx.xclbin
+cp -f ../vtd/runner/aie_reconfig_overhead/strx/aie_reconfig_overhead_strx.elf ../tools/bins/Runner/aie_reconfig_overhead/aie_reconfig_overhead_strx.elf
+cp -f ../vtd/runner/aie_reconfig_overhead/strx/profile_aie_reconfig_strx.json ../tools/bins/Runner/aie_reconfig_overhead/profile_aie_reconfig_strx.json
+cp -f ../vtd/runner/cmd_chain_latency/strx/recipe_cmd_chain_latency_strx.json ../tools/bins/Runner/cmd_chain_latency/recipe_cmd_chain_latency_strx.json
+cp -f ../vtd/runner/cmd_chain_latency/strx/cmd_chain_latency_strx.xclbin ../tools/bins/Runner/cmd_chain_latency/cmd_chain_latency_strx.xclbin
+cp -f ../vtd/runner/cmd_chain_latency/strx/profile_cmd_chain_latency_strx.json ../tools/bins/Runner/cmd_chain_latency/profile_cmd_chain_latency_strx.json
+cp -f ../vtd/runner/cmd_chain_latency/strx/nop_cmd_chain_latency_strx.elf ../tools/bins/Runner/cmd_chain_latency/nop_cmd_chain_latency_strx.elf
+cp -f ../vtd/runner/cmd_chain_throughput/strx/recipe_cmd_chain_throughput_strx.json ../tools/bins/Runner/cmd_chain_throughput/recipe_cmd_chain_throughput_strx.json
+cp -f ../vtd/runner/cmd_chain_throughput/strx/cmd_chain_throughput_strx.xclbin ../tools/bins/Runner/cmd_chain_throughput/cmd_chain_throughput_strx.xclbin
+cp -f ../vtd/runner/cmd_chain_throughput/strx/profile_cmd_chain_throughput_strx.json ../tools/bins/Runner/cmd_chain_throughput/profile_cmd_chain_throughput_strx.json
+cp -f ../vtd/runner/cmd_chain_throughput/strx/nop_cmd_chain_throughput_strx.elf ../tools/bins/Runner/cmd_chain_throughput/nop_cmd_chain_throughput_strx.elf
+cp -f ../vtd/runner/tct_all_column/recipe_tct_all_column_phx.json ../tools/bins/Runner/tct_all_column/recipe_tct_all_column_phx.json
+cp -f ../vtd/runner/tct_all_column/recipe_tct_all_column_strx.json ../tools/bins/Runner/tct_all_column/recipe_tct_all_column_strx.json
+cp -f ../vtd/runner/tct_all_column/profile_tct_all_column_phx.json ../tools/bins/Runner/tct_all_column/profile_tct_all_column_phx.json
+cp -f ../vtd/runner/tct_all_column/profile_tct_all_column_strx.json ../tools/bins/Runner/tct_all_column/profile_tct_all_column_strx.json
+cp -f ../vtd/runner/tct_all_column/tct_all_col_phx.xclbin ../tools/bins/Runner/tct_all_column/tct_all_col_phx.xclbin
+cp -f ../vtd/runner/tct_all_column/tct_all_col_strx.xclbin ../tools/bins/Runner/tct_all_column/tct_all_col_strx.xclbin
+cp -f ../vtd/runner/tct_all_column/tct_4col.elf ../tools/bins/Runner/tct_all_column/tct_4col.elf
+cp -f ../vtd/runner/tct_one_column/recipe_tct_one_column_phx.json ../tools/bins/Runner/tct_one_column/recipe_tct_one_column_phx.json
+cp -f ../vtd/runner/tct_one_column/recipe_tct_one_column_strx.json ../tools/bins/Runner/tct_one_column/recipe_tct_one_column_strx.json
+cp -f ../vtd/runner/tct_one_column/profile_tct_one_column_phx.json ../tools/bins/Runner/tct_one_column/profile_tct_one_column_phx.json
+cp -f ../vtd/runner/tct_one_column/profile_tct_one_column_strx.json ../tools/bins/Runner/tct_one_column/profile_tct_one_column_strx.json
+cp -f ../vtd/runner/tct_one_column/tct_one_col_phx.xclbin ../tools/bins/Runner/tct_one_column/tct_one_col_phx.xclbin
+cp -f ../vtd/runner/tct_one_column/tct_one_col_strx.xclbin ../tools/bins/Runner/tct_one_column/tct_one_col_strx.xclbin
+cp -f ../vtd/runner/tct_one_column/tct_1col.elf ../tools/bins/Runner/tct_one_column/tct_1col.elf
+
 if [[ $release == 1 ]]; then
   build_targets $RELEASE_BUILD_TYPE
   if [[ $nocmake == 0 ]]; then
     download_npufws
-    # Prepare xbutil validate related files for packaging
+    # Prepare xrt-smi validate related files for packaging
     mkdir -p $XBUTIL_VALIDATE_BINS_DIR
     cp -r ../tools/bins/* $XBUTIL_VALIDATE_BINS_DIR
     package_targets $RELEASE_BUILD_TYPE
@@ -259,7 +352,7 @@ if [[ $debug == 1 ]]; then
   build_targets $DEBUG_BUILD_TYPE
   if [[ $nocmake == 0 ]]; then
     download_npufws
-    # Prepare xbutil validate related files for packaging
+    # Prepare xrt-smi validate related files for packaging
     mkdir -p $XBUTIL_VALIDATE_BINS_DIR
     cp -r ../tools/bins/* $XBUTIL_VALIDATE_BINS_DIR
     package_targets $DEBUG_BUILD_TYPE
