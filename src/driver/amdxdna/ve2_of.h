bbf4688990706878b9be446c1ceb8cd95bccff29
Kaijar, Saifuddin <saifuddin.kaijar@amd.com>
2025-09-25 16:04:39 +0530

    Fixed code style issue for ve2  file v2
    Signed-off-by: Kaijar, Saifuddin <saifuddin.kaijar@amd.com>

--- src/driver/amdxdna/ve2_of.h
+++ src/driver/amdxdna/ve2_of.h
@@ -27,40 +27,40 @@ extern int partition_size;
 extern u32 ve2_hwctx_limit;
 
 struct aie_version {
-        u16 major;
-        u16 minor;
+	u16 major;
+	u16 minor;
 };
 
 struct aie_tile_metadata {
-        u16 row_count;
-        u16 row_start;
-        u16 dma_channel_count;
-        u16 lock_count;
-        u16 event_reg_count;
+	u16 row_count;
+	u16 row_start;
+	u16 dma_channel_count;
+	u16 lock_count;
+	u16 event_reg_count;
 };
 
 struct aie_metadata {
-        u32 size;
-        u16 cols;
-        u16 rows;
-        struct aie_version version;
-        struct aie_tile_metadata core;
-        struct aie_tile_metadata mem;
-        struct aie_tile_metadata shim;
+	u32 size;
+	u16 cols;
+	u16 rows;
+	struct aie_version version;
+	struct aie_tile_metadata core;
+	struct aie_tile_metadata mem;
+	struct aie_tile_metadata shim;
 };
 
 struct clock_entry {
-        char name[16];
-        u32 freq_mhz;
+	char name[16];
+	u32 freq_mhz;
 };
 
 struct ve2_config_hwctx {
-        u64     log_buf_addr;
-        u32     log_buf_size;
-        u64     debug_buf_addr;
-        u32     debug_buf_size;
-        u64     dtrace_addr;
-	u32 	opcode_timeout_config;
+	u64	log_buf_addr;
+	u32	log_buf_size;
+	u64	debug_buf_addr;
+	u32	debug_buf_size;
+	u64	dtrace_addr;
+	u32	opcode_timeout_config;
 };
 
 // Define the node struct for the FIFO queue
@@ -71,17 +71,17 @@ struct amdxdna_ctx_command_fifo {
 };
 
 struct amdxdna_ctx_priv {
-	u32			        start_col;
-	u32		        	num_col;
-	struct device		        *aie_dev;
-	struct aie_partition_init_args  *args;
-	struct ve2_hsa_queue	        hwctx_hsa_queue;
-	struct ve2_config_hwctx         hwctx_config[VE2_MAX_COL];
-	wait_queue_head_t	        waitq;
-	struct amdxdna_sched_job        *pending[HWCTX_MAX_CMDS];
-	struct timer_list	        event_timer;
-	volatile bool                   misc_intrpt_flag;
-	struct mutex                    privctx_lock; /* protect private ctx */
+	u32				start_col;
+	u32				num_col;
+	struct device			*aie_dev;
+	struct aie_partition_init_args	*args;
+	struct ve2_hsa_queue		hwctx_hsa_queue;
+	struct ve2_config_hwctx		hwctx_config[VE2_MAX_COL];
+	wait_queue_head_t		waitq;
+	struct amdxdna_sched_job	*pending[HWCTX_MAX_CMDS];
+	struct timer_list		event_timer;
+	volatile bool			misc_intrpt_flag; /* Hardware sync required */
+	struct mutex			privctx_lock; /* protect private ctx */
 };
 
 struct amdxdna_dev_priv {
@@ -91,19 +91,19 @@ struct amdxdna_dev_priv {
 };
 
 struct amdxdna_mgmtctx {
-	struct amdxdna_dev              *xdna;
-	struct amdxdna_ctx              *active_ctx;
+	struct amdxdna_dev		*xdna;
+	struct amdxdna_ctx		*active_ctx;
 	struct device			*mgmt_aiedev;
 	u32				start_col;
-	u32                             mgmt_partid;
-	struct aie_partition_init_args  args;
-	struct list_head                ctx_command_fifo_head;
-	spinlock_t                      ctx_lock; /* protect ctx add/remove/update */
-	struct work_struct              sched_work;
-	struct workqueue_struct         *mgmtctx_workq;
-	volatile u32                    is_partition_idle;
-	volatile u32                    is_context_req;
-	volatile u32                    is_idle_due_to_context;
+	u32				mgmt_partid;
+	struct aie_partition_init_args	args;
+	struct list_head		ctx_command_fifo_head;
+	spinlock_t			ctx_lock; /* protect ctx add/remove/update */
+	struct work_struct		sched_work;
+	struct workqueue_struct		*mgmtctx_workq;
+	volatile u32			is_partition_idle; /* Hardware sync required */
+	volatile u32			is_context_req; /* Hardware sync required */
+	volatile u32			is_idle_due_to_context; /* Hardware sync required */
 };
 
 struct amdxdna_dev_hdl {
