0c2a537b91b65d5f76a45fcd03239312889f4531
Saifuddin Kaijar <54270703+saifuddin-xilinx@users.noreply.github.com>
2025-10-31 16:25:13 +0530

    Fixed resolver. Added final set of changes from of_driver to here (#842)
    * Fixed resolver
    
    Signed-off-by: Saifuddin Kaijar <saifuddin.kaijar@amd.com>
    
    * Fixed code intigrity issue
    
    Signed-off-by: Saifuddin Kaijar <saifuddin.kaijar@amd.com>
    
    ---------
    
    Signed-off-by: Saifuddin Kaijar <saifuddin.kaijar@amd.com>

--- src/driver/amdxdna/ve2_mgmt.c
+++ src/driver/amdxdna/ve2_mgmt.c
@@ -61,45 +61,40 @@ static void cert_setup_partition(struct amdxdna_dev *xdna, struct device *aie_de
 static int ve2_xrs_col_list(struct amdxdna_ctx *hwctx, struct alloc_requests *xrs_req, u32 num_col)
 {
 	struct amdxdna_dev *xdna = hwctx->client->xdna;
-	u32 width = num_col;
 	int total_col = xrs_get_total_cols(xdna->dev_handle->xrs_hdl);
-	int start = 0, end = total_col - num_col;
-	int first, last, entries = 0;
-	int i;
+	int i, start;
+	int max_start = total_col - num_col;
+	int entries = 0;
 
-	if (start_col >= 0) {
+	if (start_col > 0) {
 		if (start_col + num_col > total_col) {
 			XDNA_ERR(xdna, "Invalid start_col_index %d, num col %d",
 				 start_col, num_col);
 			return -EINVAL;
 		}
-		first = start_col;
-		entries = (end - first) / width + 1;
+
+		for (start = start_col; start <= max_start; start += num_col)
+			entries++;
 	} else {
-		first = start + (width - start % width) % width;
-		last = end - end % width;
-
-		if (last >= first)
-			entries = (last - first) / width + 1;
-
-		if (!entries) {
-			XDNA_ERR(xdna, "Start %d end %d width %d", start, end, width);
-			return -EINVAL;
-		}
+		for (start = 0; start <= max_start; start += num_col)
+			entries++;
 	}
 
-	XDNA_DBG(xdna, "start %d end %d first %d last %d, entries %d",
-		 start, end, first, last, entries);
+	if (entries == 0) {
+		XDNA_ERR(xdna, "No valid start_col found for num_col %d in total_col %d",
+			 num_col, total_col);
+		return -EINVAL;
+	}
 
-	xrs_req->cdo.start_cols = kmalloc_array(entries,
-						sizeof(*xrs_req->cdo.start_cols),
+	xrs_req->cdo.start_cols = kmalloc_array(entries, sizeof(*xrs_req->cdo.start_cols),
 						GFP_KERNEL);
 	if (!xrs_req->cdo.start_cols)
 		return -ENOMEM;
 
 	xrs_req->cdo.cols_len = entries;
-	for (i = 0; i < entries; i++)
-		xrs_req->cdo.start_cols[i] = first + i * width;
+	for (i = 0, start = (start_col > 0 ? start_col : 0);
+	     start <= max_start; start += num_col, i++)
+		xrs_req->cdo.start_cols[i] = start;
 
 	print_hex_dump_debug("col_list: ", DUMP_PREFIX_OFFSET, 16, 4,
 			     xrs_req->cdo.start_cols,
@@ -136,6 +131,7 @@ int ve2_xrs_request(struct amdxdna_dev *xdna, struct amdxdna_ctx *hwctx)
 	ret = ve2_xrs_col_list(hwctx, xrs_req, xrs_req->cdo.ncols);
 	if (ret) {
 		XDNA_ERR(xdna, "Allocate XRS col resource failed, ret %d", ret);
+		mutex_unlock(&xrs->xrs_lock);
 		goto free_xrs_req;
 	}
 
@@ -144,12 +140,14 @@ int ve2_xrs_request(struct amdxdna_dev *xdna, struct amdxdna_ctx *hwctx)
 	ret = xrs_allocate_resource(xrs, xrs_req, &load_act);
 	if (ret) {
 		XDNA_ERR(xdna, "Allocate XRS resource failed, ret %d", ret);
+		mutex_unlock(&xrs->xrs_lock);
 		goto free_start_cols;
 	}
 
 	ret = ve2_create_mgmt_partition(xdna, hwctx, &load_act);
 	if (ret) {
 		XDNA_ERR(xdna, "Creating AIE partition failed, ret %d", ret);
+		mutex_unlock(&xrs->xrs_lock);
 		goto xrs_release;
 	}
 
@@ -166,7 +164,7 @@ free_start_cols:
 	kfree(xrs_req->cdo.start_cols);
 free_xrs_req:
 	kfree(xrs_req);
-	mutex_unlock(&xrs->xrs_lock);
+	XDNA_ERR(xdna, "XRS Request Failed. Ret %d", ret);
 	return ret;
 }
 
