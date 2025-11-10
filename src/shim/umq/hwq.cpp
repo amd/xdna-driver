3abd9f9e56392e505f12750630e6dcf2be2972bb
Max Zhen <max.zhen@amd.com>
2025-11-09 20:30:12 -0800

    single exec buf works
    Signed-off-by: Max Zhen <max.zhen@amd.com>

--- src/shim/umq/hwq.cpp
+++ src/shim/umq/hwq.cpp
@@ -5,33 +5,8 @@
 
 namespace {
 
-inline void
-mark_slot_invalid(volatile struct host_queue_packet *pkt)
-{
-  pkt->xrt_header.common_header.type = HOST_QUEUE_PACKET_TYPE_INVALID;
-}
-
-inline void
-mark_slot_valid(volatile struct host_queue_packet *pkt)
-{
-  /* Issue mfence instruction to make sure all writes to the slot before is done */
-  std::atomic_thread_fence(std::memory_order::memory_order_seq_cst);
-  pkt->xrt_header.common_header.type = HOST_QUEUE_PACKET_TYPE_VENDOR_SPECIFIC;
-}
-
-inline bool
-is_slot_valid(volatile struct host_queue_packet *pkt)
-{
-  return pkt->xrt_header.common_header.type == HOST_QUEUE_PACKET_TYPE_VENDOR_SPECIFIC;
-}
-
-}
-
-namespace shim_xdna {
-
 void
-hwq_umq::
-init_indirect_buf(volatile struct host_indirect_data *indirect_buf, int size)
+init_indirect_buf(volatile struct shim_xdna::host_indirect_data *indirect_buf, int size)
 {
   for (int i = 0; i < size; i++) {
     indirect_buf[i].header.type = HOST_QUEUE_PACKET_TYPE_VENDOR_SPECIFIC;
@@ -41,6 +16,18 @@ init_indirect_buf(volatile struct host_indirect_data *indirect_buf, int size)
     indirect_buf[i].header.indirect = 0;
   }
 }
+
+volatile uint32_t *
+map_doorbell(const shim_xdna::pdev& pdev, uint32_t doorbell_offset)
+{
+  return reinterpret_cast<volatile uint32_t *>(
+    pdev.mmap(0, sizeof(uint32_t), PROT_WRITE, MAP_SHARED, doorbell_offset)
+    );
+}
+
+}
+
+namespace shim_xdna {
 
 hwq_umq::
 hwq_umq(const device& dev, size_t nslots) : hwq(dev)
@@ -54,35 +41,30 @@ hwq_umq(const device& dev, size_t nslots) : hwq(dev)
   const size_t indirect_sz = (sizeof(struct host_indirect_data) * HSA_MAX_LEVEL1_INDIRECT_ENTRIES) * nslots;
 
 #ifdef UMQ_HELLO_TEST
-  const size_t umq_sz = 0x8000; // 32k, current cmd_bo max size
-  //const size_t umq_sz = 0x200000; // 2M, lift the cmd_bo max size to enable this
+  const size_t umq_sz = 0x200000;
 #else
   const size_t umq_sz = header_sz + queue_sz + indirect_sz;
 #endif
 
-  shim_debug("umq sz %ld", umq_sz);
+  shim_debug("Creating UMQ HW queue of size %ld", umq_sz);
 
-  m_umq_bo = std::make_unique<cmd_buffer>(m_pdev, umq_sz, AMDXDNA_BO_CMD);
+  m_umq_bo = std::make_unique<buffer>(m_pdev, umq_sz, AMDXDNA_BO_CMD);
   m_umq_bo_buf = m_umq_bo->vaddr();
+  std::memset(m_umq_bo_buf, 0, umq_sz);
+
   m_umq_hdr = reinterpret_cast<volatile struct host_queue_header *>(m_umq_bo_buf);
   m_umq_pkt = reinterpret_cast<volatile struct host_queue_packet *>
-    ((char *)m_umq_bo_buf + header_sz);
+    (reinterpret_cast<uintptr_t>(m_umq_bo_buf) + header_sz);
   m_umq_indirect_buf = reinterpret_cast<volatile struct host_indirect_data *>
-    ((char *)m_umq_bo_buf + header_sz + queue_sz);
+    (reinterpret_cast<uintptr_t>(m_umq_bo_buf) + header_sz + queue_sz);
 
-  // set all mapped memory to 0 
-  std::memset(m_umq_bo_buf, 0, umq_sz);
-  
   // init slots and indirect buf
-  for (int i = 0; i < nslots; i++) {
-    mark_slot_invalid(&m_umq_pkt[i]);
+  for (int i = 0; i < nslots; i++)
     init_indirect_buf(&m_umq_indirect_buf[i * HSA_MAX_LEVEL1_INDIRECT_ENTRIES], HSA_MAX_LEVEL1_INDIRECT_ENTRIES);
-  }
 
   m_umq_hdr->capacity = nslots;
   // data_address starts after header
   m_umq_hdr->data_address = m_umq_bo->get_properties().paddr + header_sz;
-
   // indirect buf starts after queue
   m_indirect_paddr = m_umq_hdr->data_address + queue_sz;
 
@@ -97,24 +79,9 @@ hwq_umq::
 
 void
 hwq_umq::
-map_doorbell(uint32_t doorbell_offset)
-{
-  m_mapped_doorbell = reinterpret_cast<volatile uint32_t *>(
-    m_pdev.mmap(0, sizeof(uint32_t), PROT_WRITE, MAP_SHARED, doorbell_offset));
-}
-
-volatile struct host_queue_header *
-hwq_umq::
-get_header_ptr() const
-{
-  return reinterpret_cast<volatile struct host_queue_header *>(m_umq_bo_buf);
-}
-
-void
-hwq_umq::
 dump() const
 {
-  auto h = get_header_ptr();
+  auto h = m_umq_hdr;
   shim_debug("Dumping UMQ queue header @%p:", h);
   shim_debug("\tRead Index:\t0x%lx", h->read_index);
   shim_debug("\tWrite Index:\t0x%lx", h->write_index);
@@ -147,20 +114,20 @@ dump() const
 
       for (int i = 0; i < HSA_MAX_LEVEL1_INDIRECT_ENTRIES; i++, hp++) {
         uint32_t hi = hp->host_addr_high;
-	uint32_t lo = hp->host_addr_low;
+        uint32_t lo = hp->host_addr_low;
         shim_debug("\thost addr: [0x%x 0x%x]", hi, lo);
 
-	volatile struct host_indirect_data *data =
-	  reinterpret_cast<volatile struct host_indirect_data *>(m_umq_indirect_buf);
-	shim_debug("\t\th:distribute:\t%d", data[i].header.distribute);
-	shim_debug("\t\th:indirect:\t%d", data[i].header.indirect);
-	shim_debug("\t\tp:dpu: [0x%x 0x%x]",
+        volatile struct host_indirect_data *data =
+        reinterpret_cast<volatile struct host_indirect_data *>(m_umq_indirect_buf);
+        shim_debug("\t\th:distribute:\t%d", data[i].header.distribute);
+        shim_debug("\t\th:indirect:\t%d", data[i].header.indirect);
+        shim_debug("\t\tp:dpu: [0x%x 0x%x]",
           data[i].payload.dpu_control_code_host_addr_high,
           data[i].payload.dpu_control_code_host_addr_low);
       }
     }
   }
-  shim_debug("dump finished\r\n");
+  shim_debug("Finished dumping UMQ\r\n");
 }
 
 void
@@ -168,89 +135,86 @@ hwq_umq::
 dump_raw() const
 {
   auto d = reinterpret_cast<volatile uint32_t *>(m_umq_pkt);
-  auto sz = get_header_ptr()->capacity * sizeof(struct host_queue_packet) / sizeof(uint32_t);
+  auto sz = m_umq_hdr->capacity * sizeof(struct host_queue_packet) / sizeof(uint32_t);
   shim_debug("Dumping raw UMQ queue slot data @%p, len=%ld WORDs:", m_umq_pkt, sz);
   for (int i = 0; i < sz; i++)
     shim_debug("0x%08x", d[i]);
+  shim_debug("Finished dumping raw UMQ queue slot data\r\n");
 }
 
-uint64_t
+uint32_t
 hwq_umq::
-reserve_slot()
+get_next_avail_slot()
 {
   uint64_t cur_slot = 0;
-  bool queue_full = false;
-  auto h = get_header_ptr();
+  auto h = m_umq_hdr;
 
   do {
-    if (h->write_index < h->read_index) {
-      shim_err(EINVAL, "Queue read before write! read_index=0x%lx, write_index=0x%lx",
-        h->read_index, h->write_index);
+    // Take a snapshot of read and write index in case they changes while being processed here
+    uint64_t wi = h->write_index;
+    uint64_t ri = h->read_index;
+
+    if (wi < ri) {
+      // Invalid queue.
       dump();
-    } else if ((h->write_index - h->read_index) < h->capacity) {
-      cur_slot = h->write_index;
-      h->write_index++;
+      shim_err(EINVAL, "UMQ was read before written! read_index=0x%lx, write_index=0x%lx", ri, wi);
+    } else if ((wi - ri) < h->capacity) {
+      // Found a slot.
+      cur_slot = wi;
       break;
     } else {
-      queue_full = true;
-    }
-
-    if (queue_full) {
       shim_debug("Queue is full, wait for next available slot");
-      //should wait for h->read_index which should be the first available slot.
-      wait_command(h->read_index, 0);
+      // The ri is the first available slot.
+      wait_command(ri, 0);
     }
-  } while (queue_full);
+  } while (true);
 
-  return cur_slot;
-}
-
-int
-hwq_umq::
-get_pkt_idx(uint64_t index)
-{
-  return index & (get_header_ptr()->capacity - 1);
+  return cur_slot & (h->capacity - 1);
 }
 
 volatile struct host_queue_packet *
 hwq_umq::
-get_pkt(uint64_t index)
+get_pkt(uint32_t index)
 {
-  auto pkt = &m_umq_pkt[get_pkt_idx(index)];
-  if (is_slot_valid(pkt)) {
-    shim_err(EINVAL, "Slot is ready before use! index=0x%lx", index);
-    dump();
-  }
-  return pkt;
+  return &m_umq_pkt[index];
 }
 
 uint64_t
 hwq_umq::
-issue_exec_buf(ert_dpu_data *dpu, uint64_t comp)
+issue_single_exec_buf(ert_dpu_data *dpu, uint64_t comp)
 {
-  auto slot_idx = reserve_slot();
-  auto pkt = get_pkt(slot_idx);
-  size_t pkt_size;
+  auto slot_idx = get_next_avail_slot();
 
   if (get_ert_dpu_data_next(dpu))
-    pkt_size = fill_indirect_exec_buf(slot_idx, pkt, dpu);
+    fill_indirect_exec_buf(slot_idx, dpu);
   else
-    pkt_size = fill_direct_exec_buf(pkt, dpu); 
+    fill_direct_exec_buf(slot_idx, dpu); 
 
+  auto pkt = get_pkt(slot_idx);
   auto hdr = &pkt->xrt_header;
   hdr->common_header.opcode = HOST_QUEUE_PACKET_EXEC_BUF;
   hdr->completion_signal = comp;
+  // TODO: remove once uC stops looking at this field.
+  hdr->common_header.type = HOST_QUEUE_PACKET_TYPE_VENDOR_SPECIFIC;
 
-  fill_slot_and_send(pkt, pkt_size);
+  /* Issue mfence instruction to make sure all writes to the slot before is done */
+  std::atomic_thread_fence(std::memory_order::memory_order_seq_cst);
+  // Indicates the slot is ready for processing by uC.
+  // Must be the last step after pkt is filled up.
+  uint64_t wi = m_umq_hdr->write_index;
+  m_umq_hdr->write_index++;
 
-  return slot_idx;
+  // Wake up uC in case it is sleeping and waiting.
+  *m_mapped_doorbell = 0;
+
+  return wi;
 }
 
-size_t
+void
 hwq_umq::
-fill_indirect_exec_buf(uint64_t slot_idx, 
-  volatile struct host_queue_packet *pkt,
-  ert_dpu_data *dpu) {
+fill_indirect_exec_buf(uint32_t slot_idx, ert_dpu_data *dpu)
+{
+  auto pkt = get_pkt(slot_idx);
   auto pkt_size = (dpu->chained + 1) * sizeof(struct host_indirect_packet_entry);
 
   if (dpu->chained + 1 >= HSA_MAX_LEVEL1_INDIRECT_ENTRIES)
@@ -267,8 +231,8 @@ fill_indirect_exec_buf(uint64_t slot_idx,
 
   for (int i = 0; dpu; i++, hp++, dpu = get_ert_dpu_data_next(dpu)) {
     auto data_size = sizeof(struct host_indirect_data) * HSA_MAX_LEVEL1_INDIRECT_ENTRIES;
-    auto prefix_off = get_pkt_idx(slot_idx) * data_size;
-    auto prefix_idx = get_pkt_idx(slot_idx) * HSA_MAX_LEVEL1_INDIRECT_ENTRIES;
+    auto prefix_off = slot_idx * data_size;
+    auto prefix_idx = slot_idx * HSA_MAX_LEVEL1_INDIRECT_ENTRIES;
     auto buf_paddr = m_indirect_paddr + prefix_off +
        sizeof(struct host_indirect_data) * i;
 
@@ -291,15 +255,16 @@ fill_indirect_exec_buf(uint64_t slot_idx,
   auto hdr = &pkt->xrt_header;
   hdr->common_header.distribute = 1;
   hdr->common_header.indirect = 1;
-
-  return pkt_size;
+  hdr->common_header.count = pkt_size;
 }
 
-size_t
+void
 hwq_umq::
-fill_direct_exec_buf(volatile struct host_queue_packet *pkt,
-                     ert_dpu_data *dpu) {
+fill_direct_exec_buf(uint32_t slot_idx, ert_dpu_data *dpu)
+{
+  auto pkt = get_pkt(slot_idx);
   auto pkt_size = sizeof(struct exec_buf);
+
   if (pkt_size > sizeof(pkt->data))
     shim_err(EINVAL, "dpu pkt_size=0x%lx > pkt_data max size=%x%lx",
       pkt_size, sizeof(pkt->data));
@@ -315,28 +280,7 @@ fill_direct_exec_buf(volatile struct host_queue_packet *pkt,
   auto hdr = &pkt->xrt_header;
   hdr->common_header.distribute = 0;
   hdr->common_header.indirect = 0;
-
-  return pkt_size;
-}
-
-void
-hwq_umq::
-fill_slot_and_send(volatile struct host_queue_packet *pkt, size_t size)
-{
-  if (size > sizeof(pkt->data))
-    shim_err(EINVAL, "HSA packet payload too big, size=0x%lx", size);
-
-  auto hdr = &pkt->xrt_header;
-  hdr->common_header.count = size;
-
-  //comment this out, debug only
-  //dump();
-
-  /* Always done as last step. */
-  mark_slot_valid(pkt);
-
-  /* Wake up CERT */
-  *m_mapped_doorbell = 0;
+  hdr->common_header.count = pkt_size;
 }
 
 uint64_t
@@ -357,14 +301,11 @@ issue_command(const cmd_buffer *cmd_bo)
     shim_err(EINVAL, "No dpu data, invalid exec buf");
   }
 
-  if (get_ert_dpu_data_next(dpu_data))
-    shim_debug("this is a multi-column dpu request.");
-
-  // Completion signal area has to be a full WORD, we utilze the command_bo
+  // Completion signal area has to be a full WORD, we utilze the command_bo header
   uint64_t comp = cmd_bo->paddr() + offsetof(ert_start_kernel_cmd, header);
+  auto seq = issue_single_exec_buf(dpu_data, comp);
 
-  auto seq = issue_exec_buf(dpu_data, comp);
-  shim_debug("Submitted command (%ld)", seq);
+  shim_debug("Submitted %s-uC command (%ld)", get_ert_dpu_data_next(dpu_data) ? "multi" : "single", seq);
   return seq;
 }
 
@@ -375,7 +316,7 @@ bind_hwctx(const hwctx& ctx)
   // link hwctx by parent class
   hwq::bind_hwctx(ctx);
   // setup doorbell mapping by child class
-  map_doorbell(ctx.get_doorbell());
+  m_mapped_doorbell = map_doorbell(m_pdev, ctx.get_doorbell());
 }
 
 void
