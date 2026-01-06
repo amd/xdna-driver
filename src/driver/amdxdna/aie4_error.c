98f924c8b21caa9663daff113b0f762f0089de72
David Zhang <yidong.zhang@amd.com>
2026-01-06 15:51:37 -0800

    enable aie4 async buffer
    Signed-off-by: David Zhang <yidong.zhang@amd.com>

--- src/driver/amdxdna/aie4_error.c
+++ src/driver/amdxdna/aie4_error.c
@@ -15,16 +15,11 @@ struct async_event {
 	struct aie4_msg_async_event_config_resp	resp;
 	struct workqueue_struct		*wq;
 	struct work_struct		work;
-	u8				*buf;
-	dma_addr_t			addr;
-	u32				size;
+	struct amdxdna_mgmt_dma_hdl	*dma_hdl;
 };
 
 struct async_events {
 	struct workqueue_struct		*wq;
-	u8				*buf;
-	dma_addr_t			addr;
-	u32				size;
 	u32				event_cnt;
 	struct async_event		event[] __counted_by(event_cnt);
 };
@@ -77,6 +72,16 @@ struct aie_err_info {
 struct aie_event_category {
 	u8			event_id;
 	enum aie_error_category category;
+};
+
+struct aie_cat_amdxdna_err_num {
+	enum aie_error_category category;
+	enum amdxdna_error_num drv_err_num;
+};
+
+struct aie_mod_amdxdna_err_mod {
+	enum aie_module_type mod_type;
+	enum amdxdna_error_module drv_err_mod;
 };
 
 #define EVENT_CATEGORY(id, cat) { id, cat }
@@ -139,6 +144,43 @@ static const struct aie_event_category aie_ml_shim_tile_event_cat[] = {
 	EVENT_CATEGORY(74U, AIE_ERROR_LOCK),
 };
 
+static const struct aie_cat_amdxdna_err_num aie_cat_err_num_map[] = {
+	{ AIE_ERROR_SATURATION, AMDXDNA_ERROR_NUM_AIE_SATURATION },
+	{ AIE_ERROR_FP, AMDXDNA_ERROR_NUM_AIE_FP },
+	{ AIE_ERROR_STREAM, AMDXDNA_ERROR_NUM_AIE_STREAM },
+	{ AIE_ERROR_ACCESS, AMDXDNA_ERROR_NUM_AIE_ACCESS },
+	{ AIE_ERROR_BUS, AMDXDNA_ERROR_NUM_AIE_BUS },
+	{ AIE_ERROR_INSTRUCTION, AMDXDNA_ERROR_NUM_AIE_INSTRUCTION },
+	{ AIE_ERROR_ECC, AMDXDNA_ERROR_NUM_AIE_ECC },
+	{ AIE_ERROR_LOCK, AMDXDNA_ERROR_NUM_AIE_LOCK },
+	{ AIE_ERROR_DMA, AMDXDNA_ERROR_NUM_AIE_DMA },
+	{ AIE_ERROR_MEM_PARITY, AMDXDNA_ERROR_NUM_AIE_MEM_PARITY },
+};
+
+static const struct aie_mod_amdxdna_err_mod aie_mod_amdxdna_err_mod_map[] = {
+	{ AIE_MEM_MOD, AMDXDNA_ERROR_MODULE_AIE_MEMORY },
+	{ AIE_CORE_MOD, AMDXDNA_ERROR_MODULE_AIE_CORE },
+	{ AIE_PL_MOD, AMDXDNA_ERROR_MODULE_AIE_PL },
+};
+
+static enum amdxdna_error_module aie_get_amdxdna_error_mod(enum aie_module_type mod_type)
+{
+	for (int i = 0; i < ARRAY_SIZE(aie_mod_amdxdna_err_mod_map); i++) {
+		if (aie_mod_amdxdna_err_mod_map[i].mod_type == mod_type)
+			return aie_mod_amdxdna_err_mod_map[i].drv_err_mod;
+	}
+	return AMDXDNA_ERROR_MODULE_UNKNOWN;
+}
+
+static enum amdxdna_error_num aie_err_cat_get_amdxdna_err_num(enum aie_error_category cat)
+{
+	for (int i = 0; i < ARRAY_SIZE(aie_cat_err_num_map); i++) {
+		if (aie_cat_err_num_map[i].category == cat)
+			return aie_cat_err_num_map[i].drv_err_num;
+	}
+	return AMDXDNA_ERROR_NUM_UNKNOWN;
+}
+
 static enum aie_error_category
 aie_get_error_category(u8 row, u8 event_id, enum aie_module_type mod_type)
 {
@@ -178,6 +220,32 @@ aie_get_error_category(u8 row, u8 event_id, enum aie_module_type mod_type)
 	return AIE_ERROR_UNKNOWN;
 }
 
+static void aie4_async_errors_cache(struct amdxdna_dev_hdl *ndev, void *err_info, u32 num_err)
+{
+	struct amdxdna_async_error *record = &ndev->async_errs_cache.err;
+	u64 current_time_us = ktime_to_us(ktime_get_real());
+	struct aie_error *errs = err_info;
+	enum amdxdna_error_module amdxdna_err_mod;
+	enum amdxdna_error_num err_num;
+	struct aie_error *err;
+	u64 err_code;
+
+	/* Cache the last async error only */
+	err = &errs[num_err - 1];
+	err_num = aie_err_cat_get_amdxdna_err_num(aie_get_error_category(err->row, err->event_id,
+									 err->mod_type));
+	amdxdna_err_mod = aie_get_amdxdna_error_mod(err->mod_type);
+	err_code = AMDXDNA_CRITICAL_ERROR_CODE_BUILD(err_num, amdxdna_err_mod);
+
+	mutex_lock(&ndev->async_errs_cache.lock);
+	record->ts_us = current_time_us;
+	record->err_code = err_code;
+	// Record tile location for last error
+	record->ex_err_code = AMDXDNA_ERROR_EXTRA_CODE_BUILD(err->row, err->col);
+
+	mutex_unlock(&ndev->async_errs_cache.lock);
+}
+
 static u32 aie4_error_backtrack(struct amdxdna_dev_hdl *ndev, void *err_info, u32 num_err)
 {
 	struct aie_error *errs = err_info;
@@ -203,8 +271,6 @@ static u32 aie4_error_backtrack(struct amdxdna_dev_hdl *ndev, void *err_info, u3
 		err_col |= (1 << err->col);
 	}
 
-	/* TODO: Send AIE error to EDAC system */
-
 	return err_col;
 }
 
@@ -226,9 +292,8 @@ static int aie4_error_async_cb(void *handle, void __iomem *data, size_t size)
 
 static int aie4_error_event_send(struct async_event *e)
 {
-	drm_clflush_virt_range(e->buf, e->size); /* device can access */
-	return aie4_register_async_event_msg(e->ndev, e->addr, e->size, e,
-					    aie4_error_async_cb);
+	amdxdna_mgmt_buff_clflush(e->dma_hdl, 0, 0);
+	return aie4_register_asyn_event_msg(e->ndev, e->dma_hdl, e, aie4_error_async_cb);
 }
 
 static void aie4_error_worker(struct work_struct *err_work)
@@ -236,6 +301,7 @@ static void aie4_error_worker(struct work_struct *err_work)
 	struct aie_err_info *info;
 	struct amdxdna_dev *xdna;
 	struct async_event *e;
+	void *vaddr;
 	u32 max_err;
 	u32 err_col;
 
@@ -248,13 +314,18 @@ static void aie4_error_worker(struct work_struct *err_work)
 
 	e->resp.status = MAX_AIE4_MSG_STATUS_CODE;
 
-	print_hex_dump_debug("AIE error: ", DUMP_PREFIX_OFFSET, 16, 4,
-			     e->buf, 0x100, false);
+	vaddr = amdxdna_mgmt_buff_get_cpu_addr(e->dma_hdl, 0);
+	if (IS_ERR(vaddr)) {
+		XDNA_ERR(xdna, "Failed to get a valid virtual addr: %ld", PTR_ERR(vaddr));
+		return;
+	}
 
-	info = (struct aie_err_info *)e->buf;
+	print_hex_dump_debug("AIE error: ", DUMP_PREFIX_OFFSET, 16, 4, vaddr, 0x100, false);
+
+	info = (struct aie_err_info *)vaddr;
 	XDNA_DBG(xdna, "Error count %d return code %d", info->err_cnt, info->ret_code);
 
-	max_err = (e->size - sizeof(*info)) / sizeof(struct aie_error);
+	max_err = (ASYNC_BUF_SIZE - sizeof(*info)) / sizeof(struct aie_error);
 	if (unlikely(info->err_cnt > max_err)) {
 		WARN_ONCE(1, "Error count too large %d\n", info->err_cnt);
 		return;
@@ -265,6 +336,8 @@ static void aie4_error_worker(struct work_struct *err_work)
 		return;
 	}
 
+	aie4_async_errors_cache(e->ndev, info->payload, info->err_cnt);
+
 	mutex_lock(&xdna->dev_handle->aie4_lock);
 	/* Re-sent this event to firmware */
 	if (aie4_error_event_send(e))
@@ -272,84 +345,84 @@ static void aie4_error_worker(struct work_struct *err_work)
 	mutex_unlock(&xdna->dev_handle->aie4_lock);
 }
 
-int aie4_error_async_events_send(struct amdxdna_dev_hdl *ndev)
-{
-	struct amdxdna_dev *xdna = ndev->xdna;
-	struct async_event *e;
-	int i, ret;
-
-	drm_WARN_ON(&xdna->ddev, !mutex_is_locked(&ndev->aie4_lock));
-	for (i = 0; i < ndev->async_events->event_cnt; i++) {
-		e = &ndev->async_events->event[i];
-		ret = aie4_error_event_send(e);
-		if (ret)
-			return ret;
-	}
-
-	return 0;
-}
-
 void aie4_error_async_events_free(struct amdxdna_dev_hdl *ndev)
 {
 	struct amdxdna_dev *xdna = ndev->xdna;
 	struct async_events *events;
+	int i;
 
+	drm_WARN_ON(&xdna->ddev, mutex_is_locked(&ndev->aie4_lock));
 	events = ndev->async_events;
 	destroy_workqueue(events->wq);
-	dma_free_noncoherent(xdna->ddev.dev, events->size, events->buf,
-			     events->addr, DMA_FROM_DEVICE);
+
+	for (i = 0; i < events->event_cnt; i++) {
+		struct async_event *e = &events->event[i];
+
+		amdxdna_mgmt_buff_free(e->dma_hdl);
+	}
 	kfree(events);
 }
 
 int aie4_error_async_events_alloc(struct amdxdna_dev_hdl *ndev)
 {
 	struct amdxdna_dev *xdna = ndev->xdna;
-	u32 total_col = ndev->total_col;
-	u32 total_size = ASYNC_BUF_SIZE * total_col;
 	struct async_events *events;
+	struct async_event *e;
 	int i, ret;
 
-	events = kzalloc(struct_size(events, event, total_col), GFP_KERNEL);
+	events = kzalloc(struct_size(events, event, ndev->total_col), GFP_KERNEL);
 	if (!events)
 		return -ENOMEM;
 
-	events->buf = dma_alloc_noncoherent(xdna->ddev.dev, total_size, &events->addr,
-					    DMA_FROM_DEVICE, GFP_KERNEL);
-	if (!events->buf) {
-		ret = -ENOMEM;
-		goto free_events;
-	}
-	events->size = total_size;
-	events->event_cnt = total_col;
-
+	events->event_cnt = ndev->total_col;
 	events->wq = alloc_ordered_workqueue("async_wq", 0);
 	if (!events->wq) {
 		ret = -ENOMEM;
-		goto free_buf;
+		goto free_events;
 	}
 
 	for (i = 0; i < events->event_cnt; i++) {
-		struct async_event *e = &events->event[i];
-		u32 offset = i * ASYNC_BUF_SIZE;
+		e = &events->event[i];
+		e->dma_hdl = amdxdna_mgmt_buff_alloc(xdna, ASYNC_BUF_SIZE, DMA_FROM_DEVICE);
+		if (IS_ERR(e->dma_hdl)) {
+			ret = PTR_ERR(e->dma_hdl);
+			goto free_buf;
+		}
 
 		e->ndev = ndev;
 		e->wq = events->wq;
-		e->buf = &events->buf[offset];
-		e->addr = events->addr + offset;
-		e->size = ASYNC_BUF_SIZE;
 		e->resp.status = MAX_AIE4_MSG_STATUS_CODE;
 		INIT_WORK(&e->work, aie4_error_worker);
 	}
 
 	ndev->async_events = events;
 
+	for (i = 0; i < ndev->async_events->event_cnt; i++) {
+		e = &ndev->async_events->event[i];
+		ret = aie4_error_event_send(e);
+		if (ret)
+			goto free_buf;
+	}
+
+	/* Just to make sure firmware handled async events */
+	ret = aie4_check_firmware_version(ndev);
+	if (ret) {
+		XDNA_ERR(xdna, "Re-query firmware version failed");
+		goto free_buf;
+	}
+
 	XDNA_DBG(xdna, "Async event count %d, buf total size 0x%x",
-		 events->event_cnt, events->size);
+		 events->event_cnt, ASYNC_BUF_SIZE);
 	return 0;
 
 free_buf:
-	dma_free_noncoherent(xdna->ddev.dev, events->size, events->buf,
-			     events->addr, DMA_FROM_DEVICE);
+	while (i) {
+		struct async_event *e = &events->event[i - 1];
+
+		amdxdna_mgmt_buff_free(e->dma_hdl);
+		--i;
+	}
+	destroy_workqueue(events->wq);
 free_events:
 	kfree(events);
 	return ret;
