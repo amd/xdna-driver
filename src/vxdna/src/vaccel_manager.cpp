6a3617c4051928a92d55a81a6c3dcc6992ce4228
Wendy Liang <wendy.liang@amd.com>
2026-01-15 08:28:57 -0800

    vxdna: udpate copyright year
    Update copyright year information of vxdna files which has changes
    in 2026.
    
    Signed-off-by: Wendy Liang <wendy.liang@amd.com>

--- src/vxdna/src/vaccel_manager.cpp
+++ src/vxdna/src/vaccel_manager.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (C) 2025, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (C) 2025 - 2026, Advanced Micro Devices, Inc. All rights reserved.
 
 /*
  * Device Manager
