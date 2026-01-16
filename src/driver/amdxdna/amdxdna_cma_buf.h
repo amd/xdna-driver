82b6e5b2cef90139e743ac810cb0f29ccb5e3fb4
Bikash Singha <bisingha@xcobisingha50x.amd.com>
2026-01-16 17:46:15 +0530

    Added cacheable bo allocation support in ve2
    Signed-off-by: Bikash Singha <bisingha@xcobisingha50x.amd.com>

--- src/driver/amdxdna/amdxdna_cma_buf.h
+++ src/driver/amdxdna/amdxdna_cma_buf.h
@@ -8,9 +8,12 @@
 
 #include <drm/drm_device.h>
 
+#define AMDXDNA_BO_FLAGS_CACHEABLE		(1U << 24)
+
 bool amdxdna_use_cma(void);
 int get_cma_mem_index(u64 flags);
-struct dma_buf *amdxdna_get_cma_buf(struct device *dev, size_t size);
+bool get_cacheable_flag(u64 flags);
+struct dma_buf *amdxdna_get_cma_buf(struct device *dev, size_t size, bool cacheable);
 struct dma_buf *amdxdna_get_cma_buf_with_fallback(struct device *const *region_devs,
 						  int max_regions,
 						  struct device *fallback_dev,
