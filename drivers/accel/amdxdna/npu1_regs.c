b8662b8dfcf70a14e613c35f1c8db80dd54428b4
Lizhi Hou <lizhi.hou@amd.com>
2026-03-04 11:38:48 -0800

    accel/amdxdna: Fix major version check on NPU1 platform
    Add the missing major number in npu1_fw_feature_table.
    
    Without the major version specified, the firmware feature check fails,
    preventing new firmware commands from being enabled on the NPU1
    platform.
    
    With the correct major version populated, the driver properly detects
    firmware support and enables the new command.
    
    Fixes: f1eac46fe5f7 ("accel/amdxdna: Update firmware version check for latest firmware")
    Signed-off-by: Lizhi Hou <lizhi.hou@amd.com>

--- drivers/accel/amdxdna/npu1_regs.c
+++ drivers/accel/amdxdna/npu1_regs.c
@@ -68,9 +68,9 @@ const struct dpm_clk_freq npu1_dpm_clk_table[] = {
 static const struct aie2_fw_feature_tbl npu1_fw_feature_table[] = {
 	{ .major = 5, .min_minor = 7 },
 #ifdef HAVE_bit_u64
-	{ .features = BIT_U64(AIE2_NPU_COMMAND), .min_minor = 8 },
+	{ .features = BIT_U64(AIE2_NPU_COMMAND), .major = 5, .min_minor = 8 },
 #else
-	{ .features = BIT_ULL(AIE2_NPU_COMMAND), .min_minor = 8 },
+	{ .features = BIT_ULL(AIE2_NPU_COMMAND), .major = 5, .min_minor = 8 },
 #endif
 	{ 0 }
 };
