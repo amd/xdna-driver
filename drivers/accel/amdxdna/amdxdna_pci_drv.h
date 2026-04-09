19c53f4cf02bbcbc5c2b6437a59f3f8fd7af3662
Max Zhen <max.zhen@amd.com>
2026-04-08 20:55:23 -0700

    accel/amdxdna: Add carveout memory support for non-IOMMU systems
    Add support for allocating buffers from reserved carveout memory when
    IOMMU is not available. This is useful during debugging or bring-up.
    
    In this configuration, the device uses physical addresses and does
    not support scatter-gather lists, requiring physically contiguous
    buffers.
    
    Implement carveout-backed allocation and integrate it into buffer
    management to support operation in physical address mode.
    
    Signed-off-by: Max Zhen <max.zhen@amd.com>

--- drivers/accel/amdxdna/amdxdna_pci_drv.h
+++ drivers/accel/amdxdna/amdxdna_pci_drv.h
@@ -176,11 +176,11 @@ void amdxdna_sysfs_fini(struct amdxdna_dev *xdna);
 
 int amdxdna_iommu_init(struct amdxdna_dev *xdna);
 void amdxdna_iommu_fini(struct amdxdna_dev *xdna);
-int amdxdna_iommu_map_bo(struct amdxdna_dev *xdna, struct amdxdna_gem_obj *abo);
-void amdxdna_iommu_unmap_bo(struct amdxdna_dev *xdna, struct amdxdna_gem_obj *abo);
 void *amdxdna_iommu_alloc(struct amdxdna_dev *xdna, size_t size, dma_addr_t *dma_addr);
 void amdxdna_iommu_free(struct amdxdna_dev *xdna, size_t size,
 			void *cpu_addr, dma_addr_t dma_addr);
+int amdxdna_dma_map_bo(struct amdxdna_dev *xdna, struct amdxdna_gem_obj *abo);
+void amdxdna_dma_unmap_bo(struct amdxdna_dev *xdna, struct amdxdna_gem_obj *abo);
 
 static inline bool amdxdna_iova_on(struct amdxdna_dev *xdna)
 {
