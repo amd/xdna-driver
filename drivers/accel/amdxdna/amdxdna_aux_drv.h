497f37c30d67befe8977071e8fda010e19d61717
Raj Kumar Rampelli <raj.kumar.rampelli@amd.com>
2026-04-24 15:14:52 +0530

    amdxdna: add VE2 auxiliary driver probe and device init
    1) Add amdxdna_aux_drv.c as the auxiliary_bus glue: DMA mask and amdxdna_dev_init
    ordering aligned with PCI.
    2) Implement ve2_init/fini in ve2_aux.c from the out-of-tree VE2 path: device handle,
    AIE topology via aie_get_device_info(), firmware load and cert broadcast, per-column fw_slots.
    3) Keep dev_ve2_info and ve2_aux_priv next to ve2_ops in ve2_aux.c
    4) Define VE2-specific struct amdxdna_dev_hdl in ve2_aux.h
    
    Signed-off-by: Raj Kumar Rampelli <raj.kumar.rampelli@amd.com>

--- drivers/accel/amdxdna/amdxdna_aux_drv.h
+++ drivers/accel/amdxdna/amdxdna_aux_drv.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (C) 2022-2026, Advanced Micro Devices, Inc.
+ * Copyright (C) 2026, Advanced Micro Devices, Inc.
  */
 
 #ifndef _AMDXDNA_AUX_DRV_H_
@@ -14,19 +14,7 @@
  * Record device static information, like reg, mbox, PSP, SMU bar index
  */
 struct amdxdna_dev_info {
-	int				reg_bar;
-	int				mbox_bar;
-	int				sram_bar;
-	int				psp_bar;
-	int				smu_bar;
-	int				doorbell_bar;
 	int				device_type;
-	int				first_col;
-	u32				dev_mem_buf_shift;
-	u64				dev_mem_base;
-	size_t				dev_mem_size;
-	const char			*default_vbnv;
-	const struct amdxdna_rev_vbnv	*rev_vbnv_tbl;
 	const struct amdxdna_dev_priv	*dev_priv;
 	const struct amdxdna_dev_ops	*ops;
 };
