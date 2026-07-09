348390ae0edde380782b0a1b5827ab37df5ca950
Hayden Laccabue <hlaccabu@amd.com>
2026-07-09 09:54:55 -0700

    accel/amdxdna: add echo bypass for arm platforms
    Signed-off-by: Hayden Laccabue <hlaccabu@amd.com>

--- drivers/accel/amdxdna/aie4_pci.c
+++ drivers/accel/amdxdna/aie4_pci.c
@@ -27,6 +27,49 @@
 #define PSP_NOTIFY_INTR		0xD007BE11
 #define AIE4_TOTAL_COLUMN	3
 
+/* Not upstreamed: echo a bypass magic to FW right after work-buffer attach for npu3a. */
+#ifdef NPU3A
+#define AIE4_MSG_OP_ECHO	0x10001
+#define MAKE_MAGIC(a, b, c, d)	((u32)((a) << 24 | (b) << 16 | (c) << 8 | (d)))
+
+struct aie4_msg_echo_req {
+	u32 val1;
+	u32 val2;
+} __packed;
+
+struct aie4_msg_echo_resp {
+	enum aie4_msg_status status;
+	u32 val1;
+	u32 val2;
+} __packed;
+
+static int aie4_iommu_bypass_echo(struct amdxdna_dev_hdl *ndev)
+{
+	DECLARE_AIE_MSG(aie4_msg_echo, AIE4_MSG_OP_ECHO);
+	struct amdxdna_dev *xdna = ndev->aie.xdna;
+	int ret;
+
+	req.val1 = MAKE_MAGIC('B', 'Y', 'P', 'A');
+	req.val2 = MAKE_MAGIC('M', 'A', 'G', 'C');
+
+	ret = aie_send_mgmt_msg_wait(&ndev->aie, &msg);
+	if (ret) {
+		XDNA_ERR(xdna, "iommu bypass echo failed, ret %d", ret);
+		return ret;
+	}
+
+	if (resp.val1 != req.val1 || resp.val2 != req.val2) {
+		XDNA_ERR(xdna, "iommu bypass echo bad value 0x%x 0x%x",
+			 resp.val1, resp.val2);
+		return -EIO;
+	}
+
+	XDNA_INFO(xdna, "iommu bypass echo ok");
+
+	return 0;
+}
+#endif /* NPU3A */
+
 /*
  * The management mailbox channel is allocated by firmware.
  * The related register and ring buffer information is on SRAM BAR.
@@ -338,6 +381,12 @@ static int aie4_pf_hw_start(struct amdxdna_dev_hdl *ndev)
 	if (ret)
 		goto mbox_fini;
 
+#ifdef NPU3A
+	ret = aie4_iommu_bypass_echo(ndev);
+	if (ret)
+		goto mbox_fini;
+#endif
+
 	ret = aie4_query_fw(ndev);
 	if (ret)
 		goto mbox_fini;
@@ -424,6 +473,12 @@ static int aie4_classic_hw_start(struct amdxdna_dev_hdl *ndev)
 				      to_buf_size(ndev->work_buf_hdl));
 	if (ret)
 		goto mbox_fini;
+
+#ifdef NPU3A
+	ret = aie4_iommu_bypass_echo(ndev);
+	if (ret)
+		goto mbox_fini;
+#endif
 
 	ret = aie4_query_fw(ndev);
 	if (ret)
