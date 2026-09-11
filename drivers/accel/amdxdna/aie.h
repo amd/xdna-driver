f6e721e75e33a57c7ff688d2afe1a4fa724d87c3
Hayden Laccabue <hlaccabu@amd.com>
2026-09-10 18:21:04 -0700

    accel/amdxdna: add auto coredump support for aie4
    Signed-off-by: Hayden Laccabue <hlaccabu@amd.com>

--- drivers/accel/amdxdna/aie.h
+++ drivers/accel/amdxdna/aie.h
@@ -36,6 +36,8 @@ struct aie_msg_ops {
 	/* Optional per-arch FW health hook; leave NULL when unsupported. */
 	int (*fill_hwctx_health)(struct amdxdna_hwctx *hwctx,
 				 struct amdxdna_drm_hwctx_entry *entry);
+	/* Optional arm firmware auto coredump. Leave NULL when unused. */
+	int (*configure_auto_coredump)(struct aie_device *aie, u32 enabled);
 
 	int  (*fw_log_init)(struct amdxdna_dev *xdna, size_t size, u32 level);
 	int  (*fw_log_config)(struct amdxdna_dev *xdna, u32 level);
